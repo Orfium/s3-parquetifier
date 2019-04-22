4c1128cbbc4d04a99bc58025167792fc94ed25e5
kostas <kostas@orfium.com>
2019-04-22 15:22:07 +0300

    Added files for pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except:
+    long_description = ''
 
 setup(
-    name='S3-Parquetifier',
-    version='0.0.1',
-    description='ETL job from CSV to Parquet in AWS S3',
+    # Application name
+    name="s3-parquetifier",
 
-    # Application author details:
-    author='Konstantinos Siaterlis',
-    author_email="kostas@orfium.com",
+    # Version number
+    version="0.0.1",
+
+    # Application author details
+    author="Konstantinos Siaterlis",
+    author_email="siaterliskonsta@gmail.com",
 
     # Packages
-    packages=["s3-parquetifier", ],
+    packages=find_packages(),
 
     # Details
-    url="https://github.com/siakon89/s3-parquetifier",
+    license="LICENSE",
+    description="ETL job from CSV to Parquet in AWS S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/Orfium/s3-parquetifier',
 
     # Dependent packages (distributions)
     install_requires=[
